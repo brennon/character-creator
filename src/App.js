8178960f7daf9a292f30b7db2f6bff1f043dc4ca
Brennon Bortz <brennon.c.bortz@leidos.com>
2021-10-04 22:35:00 -0700

    Workshop complete

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/Footer';
 function App() {
   return (
     <>
+      <div style={{backgroundColor: 'hsl(195deg, 20%, 86%)', width: "100%", position: "fixed", "bottom": 0, "top": "40%", isolation: "isolate"}}></div>
       <CharacterEditor />
       <Footer />
     </>
